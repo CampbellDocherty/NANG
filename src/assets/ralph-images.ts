51dccc007e4792ff27bfc9d2353e6ecd321b48f1
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2022-05-28 14:44:42 +0100

    Add Ralph images and riddle question

---
+++ src/assets/ralph-images.ts
@@ -0,0 +1,21 @@
+import Ralph1 from './ralph-1.jpg';
+import Ralph2 from './ralph-2.jpg';
+import Ralph3 from './ralph-3.jpg';
+import Ralph4 from './ralph-4.jpg';
+import Ralph5 from './ralph-5.jpg';
+import Ralph6 from './ralph-6.jpg';
+import Ralph7 from './ralph-7.jpg';
+import Ralph8 from './ralph-8.jpg';
+import Ralph9 from './ralph-9.jpg';
+
+export const ralphImages = [
+  Ralph1,
+  Ralph2,
+  Ralph3,
+  Ralph4,
+  Ralph5,
+  Ralph6,
+  Ralph7,
+  Ralph8,
+  Ralph9,
+];
--- src/specs/App.test.tsx
+++ src/specs/App.test.tsx
@@ -23,10 +23,6 @@ describe('When a user lands on the home page', () => {
     screen.getByText('E1 6GY');
   });
 
-  it('shows the arrow icon', () => {
-    screen.getByAltText('circle arrow to flip the cards');
-  });
-
   it('shows the nang poster', () => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     [...Array(9)].forEach((_, index) => {
