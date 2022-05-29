4a410df9dabc3cae3c6b8b1fab42d4e66f0c7da0
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2022-05-29 20:56:25 +0100

    Handle mobile and desktop with dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
+import { isMobile } from 'react-device-detect';
 import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import { TouchBackend } from 'react-dnd-touch-backend';
 import { Nang } from './Nang/Nang';
 
 const App: FC = () => {
   return (
-    <DndProvider backend={TouchBackend}>
+    <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
       <Nang />
     </DndProvider>
   );
